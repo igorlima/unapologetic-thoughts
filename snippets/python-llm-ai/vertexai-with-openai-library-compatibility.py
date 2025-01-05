3ec136e1d78c53133fbfcf8a823b92193a3eea49
Igor Lima <lima.igorribeiro@gmail.com>
2025-01-04 22:09:30 -0300

    sample for vertexai api with openai library

---
+++ snippets/python-llm-ai/vertexai-with-openai-library-compatibility.py
@@ -0,0 +1,67 @@
+# USING VERTEXAI API WITH OPENAI LIBRARY
+from openai import OpenAI
+import pudb; # pu.db
+import os
+
+"""
+# pip3 show openai
+# pip3 index versions openai
+# pip3 list
+# pip3 install --no-cache --upgrade-strategy eager -I openai==1.58.1
+
+python3 -m venv my-test-env-2025a01m04d
+source my-test-env-2025a01m04d/bin/activate
+pip3 install openai==1.58.1
+pip3 install pudb
+"""
+
+GOOGLE_PROJECT_ID=os.getenv("GOOGLE_PROJECT_ID")
+GOOGLE_LOCATION=os.getenv("GOOGLE_LOCATION")
+# https://igorlima.github.io/unapologetic-snippets/docs/languages/containerization/docker-samples#vertexai-gemini-rest-api
+VERTEXAI_TOKEN=os.getenv("VERTEXAI_TOKEN")
+client = OpenAI(
+  base_url=f"https://{GOOGLE_LOCATION}-aiplatform.googleapis.com/v1beta1/projects/{GOOGLE_PROJECT_ID}/locations/{GOOGLE_LOCATION}/endpoints/openapi",
+  api_key=VERTEXAI_TOKEN,
+)
+
+#  CHAT COMPLETIONS API
+response = client.chat.completions.create(
+  # https://cloud.google.com/vertex-ai/generative-ai/docs/multimodal/call-vertex-using-openai-library#supported-gemini-models
+  model="google/gemini-1.5-flash",
+  n=1,
+  messages=[{
+    "role": "system", "content": "You are a helpful assistant."
+  }, {
+    "role": "user",
+    "content": "Explain to me how AI works"
+  }]
+)
+print(response.choices[0].message.content)
+print('\n' + '-'*50 + '\n')
+
+# STREAMING
+response = client.chat.completions.create(
+  model="google/gemini-1.5-flash",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ],
+  stream=True
+)
+for chunk in response:
+  print(chunk.choices[0].delta.content)
+print('\n' + '-'*50 + '\n')
+
+"""
+REFERENCE:
+  Call Vertex AI models by using the OpenAI library
+    https://cloud.google.com/vertex-ai/generative-ai/docs/multimodal/call-vertex-using-openai-library
+
+HOW TO RUN THIS SCRIPT:
+({
+export GOOGLE_PROJECT_ID=xxxxxxx
+export GOOGLE_LOCATION=xxxxxxxxx
+export VERTEXAI_TOKEN=xxxxxxxx
+python3 vertexai-with-openai-library-compatibility.py
+})
+"""
